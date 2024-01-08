b54f75d7e6e8ae127f935bc2e330ae65b14f2c31
pedrim01 <peuu.aguiar@gmail.com>
2024-01-08 20:45:28 -0300

    deploy vercel first version cangastips1

--- src/components/TanstackProvider.tsx
+++ src/components/TanstackProvider.tsx
@@ -1,8 +1,14 @@
 'use client'
-
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactNode } from 'react'
 // import { ReactQueryDevtools } from 'react-query/devtools'
+
+declare module 'react-query/types/react/QueryClientProvider' {
+  interface QueryClientProviderProps {
+    children?: ReactNode
+  }
+}
+
 const queryClient = new QueryClient()
 
 export function TanstackProvider({ children }: { children: ReactNode }) {
